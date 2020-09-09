fb2724415b2b3d44906189afb5214185f004f47f
LIFE <dkrshw65@gmail.com>
2020-09-09 01:33:58 -0400

    working on dashboard

--- frontend/src/components/Pages/Dashboard/Header/Header.tsx
+++ frontend/src/components/Pages/Dashboard/Header/Header.tsx
@@ -1,9 +1,40 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 export const Header: React.FC = () => {
+  /**
+   * Image
+   */
+  const { file } = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "profile.jpg" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fixed(width: 125, height: 125) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <header className="header">
-      <div className="header__container"></div>
+      <div className="header__container">
+        <div className="header__wrapper">
+          <div className="header__search">
+            <input type="text" placeholder="Search..." />
+          </div>
+          <div className="header__profile">
+            <div className="header__invite"></div>
+            <div className="header__image">
+              <Img fixed={file.childImageSharp.fixed} />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
